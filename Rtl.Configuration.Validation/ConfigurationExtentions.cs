d093ba2329c27862743f0641e25b81c4cbb8ab68
Dmitry Semenov <ittennull@gmail.com>
2018-11-01 06:40:48 +0100

    Initial commit

---
+++ Rtl.Configuration.Validation/ConfigurationExtentions.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Rtl.Configuration.Validation
+{
+    public static class ConfigurationExtentions
+    {
+        public static IServiceCollection AddConfig<T>(this IServiceCollection services, IConfiguration configuration, string sectionName)
+            where T : class
+        {
+            var config = configuration.GetSection(sectionName).Get<T>();
+            Validator.ValidateObject(config, new ValidationContext(config), validateAllProperties: true);
+            services.AddSingleton(config);
+            return services;
+        }
+    }
+}
