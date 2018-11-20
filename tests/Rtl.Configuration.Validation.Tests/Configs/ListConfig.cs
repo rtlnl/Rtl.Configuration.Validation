779304dea3ae8ca70ac182deb378d109d04d1797
Dmitry Semenov <ittennull@gmail.com>
2018-11-20 16:41:58 +0100

    Add ability to validate top-level arrays

---
+++ tests/Rtl.Configuration.Validation.Tests/Configs/ListConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Rtl.Configuration.Validation.Tests.Configs
+{
+    class ListConfig : List<ListItemConfig>
+    {
+    }
+
+    class ListItemConfig
+    {
+        [Required]
+        public string Id { get; set; }
+    }
+}
---
+++ tests/Rtl.Configuration.Validation.Tests/ListConfigTests.cs
@@ -0,0 +1,37 @@
+﻿using Rtl.Configuration.Validation.Tests.Configs;
+using Rtl.Configuration.Validation.Tests.Helpers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Rtl.Configuration.Validation.Tests
+{
+    public class ListConfigTests
+    {
+        [Fact]
+        public async Task ValidConfigDoesntThrow()
+        {
+            var settings = new Dictionary<string, string>
+            {
+                ["test:0:Id"] = "1",
+                ["test:1:Id"] = "1",
+                ["test:2:Id"] = "1"
+            };
+
+            await TestHelpers.ValidationSucceeds<ListConfig>(settings);
+        }
+
+        [Fact]
+        public void ThrowsWhenListItemValidationFails()
+        {
+            var settings = new Dictionary<string, string>
+            {
+                ["test:0:Id"] = "1",
+                ["test:1:Id"] = "",
+                ["test:2:Id"] = "1"
+            };
+
+            TestHelpers.ValidationThrows<ListConfig>(settings);
+        }
+    }
+}
