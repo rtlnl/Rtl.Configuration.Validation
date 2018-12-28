2361d4d16b4dff3656babe9257398811caac74e8
Sipke Schoorstra <sipke@ideliverable.com>
2018-12-28 11:08:03 +0100

    Merged TryAddConfig with AddConfig

--- src/Rtl.Configuration.Validation/ServiceCollectionExtensions.cs
+++ src/Rtl.Configuration.Validation/ServiceCollectionExtensions.cs
@@ -11,6 +11,11 @@ namespace Rtl.Configuration.Validation
         public static IServiceCollection AddConfig<T>(this IServiceCollection services, IConfiguration configuration, string sectionName)
             where T : class, new()
         {
+            if (services.Any(x => x.ServiceType == typeof(IConfigureOptions<T>)))
+            {
+                return services;
+            }
+
             if (!services.Contains(new ServiceDescriptor(typeof(IStartupFilter), typeof(StartupFilter), ServiceLifetime.Transient)))
             {
                 services.AddTransient<IStartupFilter, StartupFilter>();
@@ -20,17 +25,6 @@ namespace Rtl.Configuration.Validation
             services.AddTransient<IOptionsValidator, OptionsValidator<T>>();
 
             return services;
-        }
-
-        public static IServiceCollection TryAddConfig<T>(this IServiceCollection services, IConfiguration configuration, string sectionName)
-            where T : class, new()
-        {
-            if (services.Any(x => x.ServiceType == typeof(IConfigureOptions<T>)))
-            {
-                return services;
-            }
-
-            return services.AddConfig<T>(configuration, sectionName);
         }
     }
 }
